d46f49f15ab59b0709d62839e44deea59b22222c
Neyran <maxnezerplay@gmail.com>
2026-06-23 17:39:08 +0300

    [PATCH] балансировка экономики

--- Content.Shared/_Onyx/CCVar/CCVars.Economy.cs
+++ Content.Shared/_Onyx/CCVar/CCVars.Economy.cs
@@ -8,7 +8,7 @@ public sealed partial class CCVars
     /// Интервал между выплатами зарплаты (секунды).
     /// </summary>
     public static readonly CVarDef<int> SalaryTime =
-        CVarDef.Create("economy.salary_time", 1200, CVar.SERVER | CVar.ARCHIVE);
+        CVarDef.Create("economy.salary_time", 1500, CVar.SERVER | CVar.ARCHIVE);
 
     /// <summary>
     /// Включить/отключить выплату зарплат.
@@ -20,7 +20,7 @@ public sealed partial class CCVars
     /// Глобальный множитель цен в торговых автоматах.
     /// </summary>
     public static readonly CVarDef<float> VendingPriceMultiplier =
-        CVarDef.Create("economy.vending_price_multiplier", 1.0f, CVar.SERVER | CVar.ARCHIVE);
+        CVarDef.Create("economy.vending_price_multiplier", 1.1f, CVar.SERVER | CVar.ARCHIVE);
 
     /// <summary>
     /// Глобальный множитель зарплат.
